58f6e9b25235736c19ef2052872194b99ad44d75
Terrence Asselin <terrence.asselin@lexisnexisrisk.com>
2021-01-26 10:12:09 -0600

    HPCC-24058 esdl tool build binding command
    Add build-binding command to generate bindings and bundles.
    Update xml command with option to output rolled-up interface definitions.
    
    Signed-off-by: Terrence Asselin <terrence.asselin@lexisnexisrisk.com>

--- tools/esdlcmd/esdlcmd_buildbind.cpp
+++ tools/esdlcmd/esdlcmd_buildbind.cpp
@@ -21,6 +21,7 @@
 #include "esdlcmdutils.hpp"
 #include <xpp/XmlPullParser.h>
 #include <memory>
+#include <unordered_set>
 
 using namespace xpp;
 
@@ -28,18 +29,27 @@ using namespace xpp;
 
 #define ESDL_BUILD_BINDING_ERR  23
 
-// String constants for element names in the manifest file
-#define MANIFEST_TAG_BIND_TEMPLATE  "service-script-file"
+// String constants for element names in the manifest or template file
+#define MANIFEST_TAG_BIND_TEMPLATE  "service-template-file"
 #define MANIFEST_TAG_OUTPUT         "output"
 #define MANIFEST_TAG_OUTPUT_FILE    "file"
-#define MANIFEST_TAG_TRANSFORM      "transform"
-#define MANIFEST_TAG_CRT            "CustomRequestTransform"
+
+#define TEMPLATE_TAG_TRANSFORM      "Transform"
+#define TEMPLATE_TAG_TRANSFORMS     "Transforms"
+#define TEMPLATE_TAG_CRT            "CustomRequestTransform"
+#define TEMPLATE_TAG_PRELOG         "PreLogging"
+#define TEMPLATE_TAG_BACKEND_REQ    "BackendRequest"
+#define TEMPLATE_TAG_BACKEND_RESP   "BackendResponse"
+#define TEMPLATE_TAG_ESDL_DEFNS     "Definitions"
 
 class EsdlBuildBindingCmd : public EsdlCmdCommon
 {
     protected:
         // Command-line options
         StringAttr optManifestPath;
+        bool optNoCdata = false;
+        StringAttr optService;
+        StringBuffer optIncludePath;
 
         // Manifest file as PTree
         Owned<IPropertyTree> manifest;
@@ -55,6 +65,11 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
         bool outputPublish;
         StringAttr outputPath;
         StringAttr bindTemplatePath;
+
+        // List of allowed tag names for Transform elements
+        std::unordered_set<std::string> transformTags{TEMPLATE_TAG_TRANSFORM, TEMPLATE_TAG_CRT, TEMPLATE_TAG_PRELOG, TEMPLATE_TAG_BACKEND_REQ, TEMPLATE_TAG_BACKEND_RESP};
+
+        EsdlCmdHelper cmdHelper;
 
     public:
 
@@ -143,10 +158,19 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
                 for (; !iter.done(); iter.next())
                 {
                     if (iter.matchOption(optManifestPath, ESDLOPT_MANIFEST_PATH))
-                    {
                         result = true;
-
+                    if (iter.matchFlag(optNoCdata, ESDLOPT_MANIFEST_NOCDATA))
+                        result = true;
+                    StringAttr oneOption;
+                    if (iter.matchOption(oneOption, ESDLOPT_INCLUDE_PATH) || iter.matchOption(oneOption, ESDLOPT_INCLUDE_PATH_S))
+                    {
+                        if(optIncludePath.length() > 0)
+                            optIncludePath.append(ENVSEPSTR);
+                        optIncludePath.append(oneOption.get());
+                        result = true;
                     }
+                    if (iter.matchOption(optService, ESDLOPT_SERVICE))
+                        result = true;
                 }
             }
             return result;
@@ -154,9 +178,14 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
 
         void usage() override
         {
-            printf(
-                    "    --manifest <file>    Path to manifest file.\n"
-            );
+            puts("Usage:");
+            puts("esdl build-binding [options]\n");
+            puts("Options:");
+            puts("    --manifest <file>    Path to manifest file.");
+            puts("    --no-cdata           Output binding without wrapping scripts in CDATA.");
+            puts("    -I | --include-path <path>");
+            puts("                         Search path for includes referenced in the source file. Can");
+            puts("                         be used multiple times.");
             EsdlCmdCommon::usage();
         }
 
@@ -256,7 +285,7 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
             return true;
         }
 
-        void copyOutStartTag(StartTag& stag, int& lastType, bool writeNsAttr = false)
+        void copyOutStartTag(XmlPullParser* xpp, StartTag& stag, int& lastType, bool writeNsAttr = false)
         {
             // Attributes can have ns prefixes, but copying
             // those out isn't supported (yet)
@@ -283,18 +312,24 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
 
             if (writeNsAttr)
             {
-                const char* uri = stag.getUri();
-                if (uri)
+                map< string, const SXT_CHAR* >::iterator it = xpp->getNsBegin();
+                while (it != xpp->getNsEnd())
                 {
-                    // Create the ns defn. Isn't there a cleaner way to do this?
-                    // Surprised xpp doesn't return it as an attribute or give
-                    // some other interface for pulling out the prefix.
-
-                    String qn(stag.getQName());
-                    // Pull the prefix out of the QName
-                    binding.appendf(" xmlns:%s=\"%s\"", qn.substring(0, qn.indexOf(':'))->str(), uri);
+                    if (it->first.compare("xml")!=0)
+                        binding.appendf(" xmlns:%s=\"%s\"", it->first.c_str(), it->second);
+                    it++;
                 }
             }
+
+            lastType = XmlPullParser::START_TAG;
+        }
+
+        bool isTransformTag(const char* tagname)
+        {
+            if( transformTags.find(tagname) != transformTags.end() )
+                return true;
+            else
+                return false;
         }
 
         bool copyOutTransform(XmlPullParser* xpp, StartTag& stagi, int& lastType)
@@ -308,8 +343,7 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
             // Copy out the start tag of the transform element passed in
             // Here we assume that the start tag only of a transform will
             // have a namespace definition.
-            copyOutStartTag(stagi, lastType, true);
-            lastType = XmlPullParser::START_TAG;
+            copyOutStartTag(xpp, stagi, lastType, true);
 
             // Copy the transform through to the output
             while ((type=xpp->next()) != XmlPullParser::END_DOCUMENT)
@@ -322,7 +356,7 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
 
                     xpp->readStartTag(stag);
 
-                    copyOutStartTag(stag, lastType);
+                    copyOutStartTag(xpp, stag, lastType);
                 }
                 else if (XmlPullParser::CONTENT == type)
                 {
@@ -342,7 +376,7 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
                         binding.appendf("</%s>", etag.getQName());
 
                     // If we've reached the end of this transform, then exit
-                    if (!stricmp(etag.getLocalName(), MANIFEST_TAG_TRANSFORM) || !stricmp(etag.getLocalName(), MANIFEST_TAG_CRT))
+                    if(isTransformTag(etag.getLocalName()))
                     {
                         // newline to separate the transforms
                         // maybe this should be optional or removed?
@@ -376,7 +410,7 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
             // In future, extract @transform name and load just that transform
 
             // Also in the future we will need to analyze the path here before loading
-            //  - determine if it is a compelte path to file
+            //  - determine if it is a complete path to file
             //  - if so, load it
             //  - if not, use the include paths from the manifest to find the file.
             //
@@ -404,7 +438,7 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
                     xppi->readStartTag(stagi);
                     // Use the localName here so that we don't need to
                     // be tied to a specific namespace
-                    if (!stricmp(stagi.getLocalName(), MANIFEST_TAG_TRANSFORM) || !stricmp(stagi.getLocalName(), MANIFEST_TAG_CRT))
+                    if(isTransformTag(stagi.getLocalName()))
                     {
                         result = copyOutTransform(xppi.get(), stagi, lastTypei);
                     }
@@ -423,7 +457,172 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
             return result;
         }
 
-        // The binding template is the shell
+        bool outputScriptSection(XmlPullParser* xpp, StartTag& stag, int& lastType)
+        {
+            if(!optNoCdata)
+                binding.append("<Scripts>");
+            binding.append("<![CDATA[");
+
+            copyOutStartTag(xpp, stag, lastType); // <Transforms>
+
+            int type;
+            //StartTag stag;
+            EndTag etag;
+            bool done = false;
+            bool result = true;
+
+            // Copy the document through to the output, replacing import statements
+            // with the transforms from the referenced files
+            while ((type=xpp->next()) != XmlPullParser::END_DOCUMENT && !done)
+            {
+                if (XmlPullParser::START_TAG == type)
+                {
+                    // close off the last start tag
+                    if (XmlPullParser::START_TAG==lastType)
+                        binding.append(">");
+
+                    xpp->readStartTag(stag);
+                    // Use the localName here so that we don't need to
+                    // be tied to a specific namespace
+                    const char* lname = stag.getLocalName();
+                    if (strieq(lname, "include"))
+                    {
+                        result = loadInclude(xpp, stag, lastType);
+                    }
+                    else
+                    {   // assume it is transform content inline, we could be more strict here
+                        copyOutStartTag(xpp, stag, lastType);
+                    }
+                }
+                else if (XmlPullParser::CONTENT == type)
+                {
+                    // close off the last start tag
+                    if (XmlPullParser::START_TAG==lastType)
+                        binding.append(">");
+
+                    binding.append(xpp->readContent());
+                    lastType=type;
+                }
+                else if (XmlPullParser::END_TAG == type)
+                {
+                    xpp->readEndTag(etag);
+                    // For tags with no children use the <tag ... /> format
+                    if (XmlPullParser::START_TAG == lastType)
+                        binding.append("/>");
+                    else
+                        binding.appendf("</%s>", etag.getQName());
+
+                    if (strieq(etag.getLocalName(), "Transforms"))
+                        done = true;
+
+                    lastType=type;
+                }
+            }
+
+            binding.append("]]>");
+            if(!optNoCdata)
+                binding.append("</Scripts>");
+
+            return result;
+        }
+
+        bool outputESDLInterfaceDefinitions(XmlPullParser* xpp, StartTag& stag, int& lastType)
+        {
+            bool result = true;
+            int type;
+            EndTag etag;
+            bool done = false;
+
+            binding.appendf("<%s><![CDATA[", TEMPLATE_TAG_ESDL_DEFNS);
+
+            // Copy the document through to the output, replacing import statements
+            // with the transforms from the referenced files
+            while ((type=xpp->next()) != XmlPullParser::END_DOCUMENT && !done)
+            {
+                if (XmlPullParser::START_TAG == type)
+                {
+                    // close off the last start tag
+                    if (XmlPullParser::START_TAG==lastType)
+                        binding.append(">");
+
+                    xpp->readStartTag(stag);
+                    // Use the localName here so that we don't need to
+                    // be tied to a specific namespace
+                    const char* lname = stag.getLocalName();
+                    if (strieq(lname, "include"))
+                    {
+                        const char* file = stag.getValue("file");
+
+                        if (!file)
+                        {
+                            const char* thisTag = stag.toString().c_str();
+                            throw MakeStringException(ESDL_BUILD_BINDING_ERR, "Failed loading definition include. Bundle template missing @file attribute in element:%s, file line:%d column:%d.", thisTag, xpp->getLineNumber(), xpp->getColumnNumber());
+                        }
+
+                        StringBuffer esxml;
+                        StringBuffer effectiveService(optService.get());
+                        StringBuffer effectiveIncludePath(optIncludePath.str());
+                        const char* service = stag.getValue("service");
+                        const char* includes = stag.getValue("searchPaths");
+
+                        // When command-line options aren't populated, use what may have been
+                        // provided as attributes in the <include> tag
+                        if (isEmptyString(optService.get()))
+                            effectiveService.set(service);
+                        if (isEmptyString(optIncludePath.str()))
+                            effectiveIncludePath.set(includes);
+
+                        cmdHelper.getServiceESXDL(file, effectiveService.str(), esxml, 0, NULL, (DEPFLAG_INCLUDE_TYPES & ~DEPFLAG_INCLUDE_METHOD), effectiveIncludePath.str(), optTraceFlags());
+                        if (esxml.length()==0)
+                        {
+                            throw MakeStringException(ESDL_BUILD_BINDING_ERR, "ESDL Definition for service %s could not be loaded from: %s", effectiveService.str(), file);
+                        }
+                        binding.append(esxml);
+                    }
+                    else
+                    {
+                        // allow any other content- copy through
+                        copyOutStartTag(xpp, stag, lastType);
+                    }
+                }
+                else if (XmlPullParser::CONTENT == type)
+                {
+                    // close off the last start tag
+                    if (XmlPullParser::START_TAG==lastType)
+                        binding.append(">");
+
+                    binding.append(xpp->readContent());
+                    lastType=type;
+                }
+                else if (XmlPullParser::END_TAG == type)
+                {
+                    xpp->readEndTag(etag);
+                    if (strieq(etag.getLocalName(), TEMPLATE_TAG_ESDL_DEFNS))
+                    {
+                        binding.append("]]>");
+                        done = true;
+                    }
+
+                    // Eat the end tag of the include
+                    if (strieq(etag.getLocalName(), "include"))
+                        continue;
+
+                    // For tags with no children use the <tag ... /> format
+                    if (XmlPullParser::START_TAG == lastType)
+                        binding.append("/>");
+                    else
+                        binding.appendf("</%s>", etag.getQName());
+                    lastType=type;
+                }
+            }
+
+            return result;
+        }
+
+        // The binding template outlines what the binding will look
+        // like. Here we generate the final binding by loading and
+        // printing out any included templates inline.
+
         bool processBindingTemplate()
         {
             bool result = true;
@@ -449,17 +648,18 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
                     if (XmlPullParser::START_TAG == type)
                     {
                         // close off the last start tag
-                        if (XmlPullParser::START_TAG==lastType)
+                        if (XmlPullParser::START_TAG == lastType)
                             binding.append(">");
 
                         xpp->readStartTag(stag);
                         // Use the localName here so that we don't need to
                         // be tied to a specific namespace
-                        if (!stricmp(stag.getLocalName(), "include"))
-                            result = loadInclude(xpp.get(), stag, lastType);
-                        else
-                        {
-                            copyOutStartTag(stag, lastType);
+                        if (strieq(stag.getLocalName(), TEMPLATE_TAG_TRANSFORMS)) {
+                            result = outputScriptSection(xpp.get(), stag, lastType);
+                        } else if (strieq(stag.getLocalName(), TEMPLATE_TAG_ESDL_DEFNS)) {
+                            result = outputESDLInterfaceDefinitions(xpp.get(), stag, lastType);
+                        } else {
+                            copyOutStartTag(xpp.get(), stag, lastType);
                             lastType=type;
                         }
                         // lastType is updated by reference by the functions called here
@@ -467,7 +667,7 @@ class EsdlBuildBindingCmd : public EsdlCmdCommon
                     else if (XmlPullParser::CONTENT == type)
                     {
                         // close off the last start tag
-                        if (XmlPullParser::START_TAG==lastType)
+                        if (XmlPullParser::START_TAG == lastType)
                             binding.append(">");
 
                         binding.append(xpp->readContent());
